508b72cd1e028a4a6df91fb1837e809975243b59
Sangbin Lee <leesangbin@outlook.com>
2023-08-23 01:45:09 +0900

    add type autocompletion accepting string in type template

--- src/templates/type.ts
+++ src/templates/type.ts
@@ -3,7 +3,7 @@ import { Config } from '@/config';
 type TypeTemplateConfig = Extract<Config['output'], { mode: 'type' }>;
 
 export const typeTemplate = (config: TypeTemplateConfig, paths: string[]) => {
-    return `export type ${config.name} = ${paths.map(path => `'${path}'`).join(' | ')};
+    return `export type ${config.name} = ${paths.map(path => `'${path}'`).join(' | ')} | (string & {});
 export type ${config.name}WithParam = ${config.name} | \`\${${config.name}}?\${string}\`;
 `;
 };
