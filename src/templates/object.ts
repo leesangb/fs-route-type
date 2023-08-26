fee08a050dcaea6012932f99c166ea83a2b64c75
유병각 <bgyu@miridih.com>
2023-08-27 00:45:35 +0900

    feat: Apply logic to group the resulting objects when the split attribute is true.
    - Add `createObject` Function
    - Add `createKey` Function

--- src/templates/object.ts
+++ src/templates/object.ts
@@ -2,12 +2,53 @@ import { Config } from '@/config';
 import { camelCase, pascalCase, constantCase, snakeCase, paramCase } from 'change-case';
 
 type ObjectTemplateConfig = Extract<Config['output'], { mode: 'object' }>;
+type GroupedPaths = Record<string, string[]>;
+
+function createKey(config: ObjectTemplateConfig, path:string, groupName?: string) {
+    if (groupName) {
+        const regex = new RegExp(`^/${groupName}`);
+        const temp = path.replace(regex, '');
+        return changeCase(temp === '' ? `/${config.rootName}` : temp, config.case);
+    } else {
+        return changeCase(path === '/' ? `/${config.rootName}` : path, config.case);
+    }
+}
+
+function createObject(config: ObjectTemplateConfig, paths: string[], groupName?: string) {
+    return `export const ${changeCase(`/${groupName}`, 'pascal')}${config.name} = {
+    ${paths.map(path => `${createKey(config, path, groupName)}: '${path}'`).join(',\n    ')},
+} as const;`;
+}
+
+const groupingPaths = (paths: string[]): GroupedPaths => {
+    const groupedPaths: GroupedPaths = {};
+
+    for (const path of paths) {
+        if (path === '/') {
+            groupedPaths[''] = ['/'];
+            continue;
+        }
+
+        const pathElements = path.split('/');
+        const category = pathElements[1];
+
+        if (category) {
+            if (!groupedPaths[category]) {
+                groupedPaths[category] = [];
+            }
+            groupedPaths[category]!.push(path);
+        }
+    }
+
+    return groupedPaths;
+};
 
 export const objectTemplate = (config: ObjectTemplateConfig, paths: string[]) => {
-    return `export const ${config.name} = {
-    ${paths.map(path => `${changeCase(path === '/' ? `/${config.rootName}` : path, config.case)}: '${path}'`).join(',\n    ')},
-} as const;
-`;
+    if (config.split) {
+        const groupedPaths = groupingPaths(paths);
+        return Object.keys(groupedPaths).map((groupName) => createObject(config, groupedPaths[groupName]!, groupName)).join('\n\n');
+    }
+    return createObject(config, paths);
 };
 
 const changeCase = (path: string, caseStyle: ObjectTemplateConfig['case']): string => {
